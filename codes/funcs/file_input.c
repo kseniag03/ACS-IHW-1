db4e1faba8aa9e0fb13b4900b039c4927c287701
kseniag03 <114473740+kseniag03@users.noreply.github.com>
2022-10-23 01:09:22 +0300

    Update file_input.c

--- codes/funcs/file_input.c
+++ codes/funcs/file_input.c
@@ -8,22 +8,22 @@ int file_input(int *n,  char *filename) {
     int i;
     FILE *file;
     if ((file = fopen(filename, "r")) == NULL) {
-        printf("Невозможно открыть файл '%s'\n", filename);
+        printf("Unable to open file '%s'\n", filename);
         return 1;
     }
     if (fscanf(file, "%d", n) < 1) {
-        printf ("Ошибка чтения из файла '%s'\n", filename);
+        printf ("Reading file '%s' error\n", filename);
         fclose(file);
         return 1;
     }
     if (*n <= 0 || *n > SIZEMAX) {
-        printf("Кол-во эл-в массива должно быть от 1 до %d\n", SIZEMAX);
+        printf("The num of elems in arr must be from 1 to %d\n", SIZEMAX);
         fclose(file);
         return 1;
     }
     for (i = 0; i < *n; ++i) {
         if (fscanf(file, "%d", &ARRAY_A[i]) < 1) {
-            printf("Ошибка чтения из файла '%s'\n", filename);
+            printf("Reading file '%s' error\n", filename);
             fclose(file);
             return 1;
         }

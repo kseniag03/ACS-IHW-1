7da6a61ec91d57a8528eff722448944d8550c39a
kseniag03 <114473740+kseniag03@users.noreply.github.com>
2022-10-23 23:36:34 +0300

    Update random_generation.s

--- codes/edited-asm-code/random_generation.s
+++ codes/edited-asm-code/random_generation.s
@@ -1,50 +1,53 @@
-	.intel_syntax noprefix
-	.text
-	.globl	random_generation
-	.type	random_generation, @function
+.intel_syntax noprefix
+.text
+.globl	random_generation
+.type	random_generation, @function
+
 random_generation:
 	push	rbp
 	mov	rbp, rsp
 	sub	rsp, 32
 	mov	QWORD PTR -24[rbp], rdi
-	mov	edi, 0
-	call	time@PLT
-	mov	edi, eax
-	call	srand@PLT
-	call	rand@PLT
-	mov	ecx, DWORD PTR SIZEMAX[rip]
-	cdq
-	idiv	ecx
+
+	mov	edi, 0				# 1-й агрумент -- 0 (NULL)
+	call	time@PLT			# time(NULL)
+	mov	edi, eax			# 1-й агрумент -- возвращаемое знач. из time(NULL)
+	call	srand@PLT			# srand(time(NULL))
+	call	rand@PLT			# вызов ф-и rand()
+	mov	ecx, DWORD PTR SIZEMAX[rip]	# ecx := SIZEMAX
+	idiv	ecx				# % SIZEMAX
+
+	mov	rax, QWORD PTR -24[rbp]		# rax := *n
+	mov	DWORD PTR [rax], edx		# n = edx (rand())
+	mov	eax, DWORD PTR [rax]		# eax := n
+	test	eax, eax			# логическое сравнение (< 1)
+	jg	.TOLOOP				# если *n > 1, переход к объявлению i и циклу (TOLOOP)
 	mov	rax, QWORD PTR -24[rbp]
-	mov	DWORD PTR [rax], edx
-	mov	rax, QWORD PTR -24[rbp]
-	mov	eax, DWORD PTR [rax]
-	test	eax, eax
-	jg	.L2
-	mov	rax, QWORD PTR -24[rbp]
-	mov	eax, DWORD PTR [rax]
-	lea	edx, 1[rax]
-	mov	rax, QWORD PTR -24[rbp]
-	mov	DWORD PTR [rax], edx
-.L2:
-	mov	DWORD PTR -4[rbp], 0
-	jmp	.L3
-.L4:
-	call	rand@PLT
-	mov	ecx, DWORD PTR VALUEMAX[rip]
-	cdq
-	idiv	ecx
-	mov	ecx, edx
-	mov	eax, DWORD PTR -4[rbp]
-	cdqe
-	lea	rdx, 0[0+rax*4]
-	lea	rax, ARRAY_A[rip]
-	mov	DWORD PTR [rdx+rax], ecx
-	add	DWORD PTR -4[rbp], 1
-.L3:
-	mov	rax, QWORD PTR -24[rbp]
-	mov	eax, DWORD PTR [rax]
-	cmp	DWORD PTR -4[rbp], eax
-	jl	.L4
-	leave
-	ret
+	mov	eax, DWORD PTR [rax]		# eax := n
+	lea	edx, 1[rax]			# ++*n
+	mov	rax, QWORD PTR -24[rbp]		# rax := *n
+	mov	DWORD PTR [rax], edx		# n = edx
+
+.TOLOOP:
+	mov	r12d, 0				# i = 0
+	jmp	.LOOP				# переход к циклу LOOP
+
+.GENERATEELEM:
+	call	rand@PLT			# вызов ф-и rand()
+	mov	ecx, VALUEMAX[rip]		# ecx := VALUEMAX
+	idiv	ecx				# % VALUEMAX
+	mov	ecx, edx			#
+	mov	eax, r12d			#
+
+	lea	rdx, 0[0 + rax * 4]		# 		
+	lea	rax, ARRAY_A[rip]		#	
+	mov	DWORD PTR [rdx + rax], ecx	# 	
+	add	r12d, 1				# ++i
+
+.LOOP:
+	mov	rax, QWORD PTR -24[rbp]		# rax := *n
+	mov	eax, DWORD PTR [rax]		# eax := n
+	cmp	r12d, eax			# сравнение i и n
+	jl	.GENERATEELEM			# если i < n, переход к GENERATEELEM
+	leave					# очистка стека
+	ret					# выполняется выход из программы
